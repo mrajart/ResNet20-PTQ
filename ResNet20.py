564989661c13203111da9935e1268f3b96455c29
Roman Masterov <mrajart@gmail.com>
2022-02-28 01:45:44 +0300

    Финальное решение

--- ResNet20.py
+++ ResNet20.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
+from torch.quantization import QuantStub, DeQuantStub
 
 def _weights_init(m):
     classname = m.__class__.__name__
@@ -12,90 +13,156 @@ def _weights_init(m):
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight)
 
-class LambdaLayer(nn.Module):
-    def __init__(self, lambd):
-        super(LambdaLayer, self).__init__()
-        self.lambd = lambd
-
-    def forward(self, x):
-        return self.lambd(x)
+def conv3x3(in_channels, out_channels, stride=1):
+    """ 3x3 Conv2d    """
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,stride=stride, padding=1, bias=False)
 
 class BasicBlock(nn.Module):
     """Basic Block для resnet 20"""
 
-    expansion = 1
-
-    def __init__(self, in_channels, out_channels, stride=1):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super().__init__()
 
-        # residual function
-        self.residual_function = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels * BasicBlock.expansion)
-        )
-
-        # shortcut
-        self.shortcut = nn.Sequential()
-
-        if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
-            # Для CIFAR10 в статье реализована опция A identity shortcuts просто padding, чтобы размерности сошлись
-            self.shortcut = LambdaLayer(lambda x:
-                                        F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, BasicBlock.expansion * out_channels // 4, BasicBlock.expansion * out_channels // 4), "constant",   0))
-        # FloatFunction()
-        self.skip_add = nn.quantized.FloatFunctional()
-
-    def forward(self, x):
-        out = self.residual_function(x)
+        #residual function
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         
-        ind = self.shortcut(x)
-        out = self.skip_add.add(out, ind)
-        #out += self.shortcut(x)
-        out = F.relu(out)
+        self.downsample = downsample
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
         return out
 
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_channels = 16
-
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
-        self.linear = nn.Linear(64, num_classes)
-        
-        self.quant = torch.quantization.QuantStub()
-        self.dequant = torch.quantization.DeQuantStub()
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, num_blocks[0])
+        self.layer2 = self.make_layer(block, 32, num_blocks[1], 2)
+        self.layer3 = self.make_layer(block, 64, num_blocks[2], 2)
+        self.fc = nn.Linear(64, num_classes)
 
         self.apply(_weights_init)
 
-    def _make_layer(self, block, out_channels, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+    def make_layer(self, block, out_channels, num_blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels))
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride))
-            self.in_channels = out_channels * block.expansion
-
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, num_blocks):
+            layers.append(block(out_channels, out_channels))
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = self.quant(x)
-        
-        out = F.relu(self.bn1(self.conv1(out)))
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = F.avg_pool2d(out, out.size()[3])
         out = out.view(out.size(0), -1)
-        out = self.linear(out)
+        out = self.fc(out)
+        return out
+    
+class QuantizedResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None,quantized=True):
+        super(QuantizedResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+        self.quantized = quantized
+        self.skip_add = nn.quantized.FloatFunctional()
+        self.quant = QuantStub()
+        self.dequant = DeQuantStub()
         
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        if self.quantized:
+            out = self.skip_add.add(out, residual)
+        else:
+            out += residual  
+        out = self.relu(out)
+        return out
+
+
+class QuantizedResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super(QuantizedResNet, self).__init__()
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer2 = self.make_layer(block, 32, layers[1], 2)
+        self.layer3 = self.make_layer(block, 64, layers[2], 2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, num_classes)
+        self.quant = QuantStub()
+        self.dequant = DeQuantStub()
+        
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.quant(x)
+        out = self.conv(out)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
         out = self.dequant(out)
         return out
+
 
 def resnet20():
     '''  ResNet 20 '''
     return ResNet(BasicBlock, [3, 3, 3, 3])
+
+def quantized_resnet20():
+    '''  ResNet 20 '''
+    return QuantizedResNet(QuantizedResidualBlock, [3, 3, 3, 3])
